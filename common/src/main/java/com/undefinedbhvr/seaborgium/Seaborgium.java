9fb1635bee3565791133044010ae47517b9459dc
UndefinedBHVR <undefinedbhvr@gmail.com>
2023-04-30 16:57:14 -0500

    Use slf4j instead of the old logger.

--- common/src/main/java/com/undefinedbhvr/seaborgium/Seaborgium.java
+++ common/src/main/java/com/undefinedbhvr/seaborgium/Seaborgium.java
@@ -1,14 +1,15 @@
 package com.undefinedbhvr.seaborgium;
 
 import com.undefinedbhvr.seaborgium.cache.CachedShaderProgram;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
-import java.util.logging.Logger;
 
 public class Seaborgium {
     private static final HashMap<Integer, CachedShaderProgram> cached_shaders = new HashMap<>();
 
-    public static Logger LOGGER = Logger.getLogger("seaborgium");
+    public static Logger LOGGER = LoggerFactory.getLogger("Seaborgium");
     public static HashMap<Integer, CachedShaderProgram> get_cached_shaders() {
         return cached_shaders;
     }
